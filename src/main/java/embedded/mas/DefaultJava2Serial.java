aecbff482eda9c5cfab40498d07646c1a5300dbd
maiquel <maiquelb@gmail.com>
2020-09-26 11:12:24 -0300

    improve build.gradle

--- src/main/java/embedded/mas/DefaultJava2Serial.java
+++ src/main/java/embedded/mas/DefaultJava2Serial.java
@@ -1,11 +1,27 @@
 package embedded.mas;
 
-public class DefaultJava2Serial implements IJava2Serial {
+import arduino.Arduino;
 
+public class DefaultJava2Serial implements IJava2Serial {
+	
+	
+	private Arduino connection;
+	
+	public DefaultJava2Serial(String port, int baudrate){
+		connection = new Arduino(port, baudrate);
+		connection.openConnection();
+	}
+	
 	@Override
 	public void send(String message) {
-		System.out.println("sending " + message);
+		//System.out.println("sending:: " + message);
+		//connection.serialWrite(message, 2000);
+		connection.serialWrite(message);
 
+	}
+	
+	public void send(char c) {
+		connection.serialWrite(c, 2000);
 	}
 
 	@Override
