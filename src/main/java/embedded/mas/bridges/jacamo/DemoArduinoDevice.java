d32d458b3ae48ca5b2b4c024ca526c49cbf7e260
VitorFurio <vitorluis.babireskifurio@gmail.com>
2021-02-03 12:50:49 -0300

    Inicio do tratamento dos dados Json

--- src/main/java/embedded/mas/bridges/jacamo/DemoArduinoDevice.java
+++ src/main/java/embedded/mas/bridges/jacamo/DemoArduinoDevice.java
@@ -1,11 +1,16 @@
 package embedded.mas.bridges.jacamo;
 
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import embedded.mas.bridges.jacamo.ArduinoDevice;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
 import jason.asSyntax.Atom;
-import jason.asSyntax.Literal;
+import sun.tools.jstat.Literal;
 
 
 /**
@@ -23,13 +28,28 @@ public class DemoArduinoDevice extends ArduinoDevice {
 
 	@Override
 	public Collection<Literal> getPercepts() {
-		String s = arduino.serialRead(); //lê o arduino
+		String json = arduino.serialRead(); //lê o arduino
+		ArrayList<Literal> percepts = new ArrayList<Literal>(); //adicionar os valores lidos arduino na lista percepts (dúvidas - olhar DemoDevice)
 		
-		//transformar a string lida do arduino em crença
-		ArrayList<Literal> percepts = new ArrayList<Literal>();
-		//adicionar os valores lidos arduino na lista percepts (dúvidas - olhar DemoDevice)
-		//percepts.add(Literal.parseLiteral(s)); 
+		JsonReader reader = Json.createReader(new ByteArrayInputStream(json.getBytes()));
+    	JsonObject jsonObject = reader.readObject(); //transformar a string em um "objeto JSON"
+    	
+    	for(String key: jsonObject.keySet()) { //iterar sobre todos os elementos do JsonObject - a variável "key" armazena cada chave do objeto json    		
+    		Object value = jsonObject.get(key); //obtém o valor associado à chave "key"
+    		String belief = key +"(";
+    		if(!(value instanceof JsonArray)) //se o valor não for um vetor (ou seja, se for uma belief com apenas um valor)
+    		   belief = belief + value;
+    		else { //se for um vetor [v1,v2,...,vn], monta uma belief key(v1,v2,...,vn)    			
+    				belief = belief + value.toString().replace("[","").replace("]", "");	 	
+    		}
+    		belief = belief + ")";
+    		
+    		//System.out.println(belief);
+    		percepts.add(Literal.parseLiteral(belief));
+    	}
+		
 		return percepts;
 	}
-
+	
+	
 }
