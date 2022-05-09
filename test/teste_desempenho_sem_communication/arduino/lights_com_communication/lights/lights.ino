fc6639681be03e4c9e69352c6a0fdb27e330bd62
79cba9613e9d8254093a28aebf8262a8b346d5d6 <maiquelb@gmail.com>
2022-05-09 09:43:07 -0300

    test performance of communication protocol

---
+++ test/teste_desempenho_sem_communication/arduino/lights_com_communication/lights/lights.ino
@@ -0,0 +1,57 @@
+#include<Embedded_Protocol_2.h>
+#include<ArduinoJson.h>
+
+Communication communication;
+const int capacity = JSON_OBJECT_SIZE(6);
+StaticJsonDocument<capacity> doc;
+
+const int LIGHT = 10;
+
+int delay_time;
+int light_state = 1;
+
+void setup() 
+{
+  pinMode(LIGHT,OUTPUT);
+  digitalWrite(LIGHT,1);  //light starts on
+     
+  Serial.begin(9600);
+  communication.startBelief("light_state");
+  communication.beliefAdd(1);
+  communication.endBelief();
+  communication.sendMessage();
+  delay(10000); //wait 30 seconds (to set up the multi-agent system)
+}
+
+void loop() 
+{
+  while(Serial.available() > 0){ //check whether there is some information from the serial (possibly from the agent)
+     String s = Serial.readString();
+     if(s.equals("light_on")){ //if the agent sends "light_on", then switch the light on
+        digitalWrite(LIGHT,1); 
+        light_state = 1; 
+        communication.startBelief("light_state");
+        communication.beliefAdd(1);
+        communication.endBelief();
+        communication.sendMessage();
+     } 
+   }  
+  
+  
+  
+  if(light_state==1){ 
+    /* wait a random time and switch the light off if it is on */
+     delay_time = 1000;
+     delay(delay_time);
+     digitalWrite(LIGHT,0);
+     light_state = 0;
+     communication.startBelief("light_state");
+     communication.beliefAdd(0);
+     communication.endBelief();
+     communication.sendMessage();
+  }
+  
+  
+ 
+    
+}
