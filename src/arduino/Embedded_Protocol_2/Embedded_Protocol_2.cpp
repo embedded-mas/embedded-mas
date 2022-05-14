64701a441b10a5b661012e76970f7554d0fc3bf9
FelipeBortolini1 <felipe.bortolini@integrejr.com.br>
2022-05-13 21:38:38 -0300

    Embedded_Protocol_2_with_ROS

--- src/arduino/Embedded_Protocol_2/Embedded_Protocol_2.cpp
+++ src/arduino/Embedded_Protocol_2/Embedded_Protocol_2.cpp
@@ -1,18 +1,20 @@
 /*
  *  Embedded_Protocol_2.cpp
- *  
+ *
  *  T: biblioteca usada pelo atuador (arduino) para mandar os dados "para cima"
  */
 
 #include "Arduino.h"
 #include "Embedded_Protocol_2.h"
-
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <string.h>
 
 void Communication::startBelief(String beliefName){
     if(_allBeliefs.length() != 0){    // se não for o primeiro elemento adiciona uma virgula.
       _allBeliefs.concat(",");
     }
-    
+
     _beliefBuffer = "";
     _beliefBuffer.concat("\"");
     _beliefBuffer.concat(beliefName);
@@ -34,19 +36,19 @@ void Communication::beliefAdd(String msg){
 }
 
 void Communication::beliefAdd(int Int){
-	_beliefBuffer.concat(String(Int));
-	_beliefBuffer.concat(',');
+    _beliefBuffer.concat(String(Int));
+    _beliefBuffer.concat(',');
 }
 
 void Communication::beliefAdd(float Float){
-        _beliefBuffer.concat(String(Float, 3));
-	_beliefBuffer.concat(',');
+    _beliefBuffer.concat(String(Float, 3));
+    _beliefBuffer.concat(',');
 }
 
 void Communication::beliefAdd(double Double){ // adiciona um parametro do tipo double a crença.
-     _beliefBuffer.concat(String(Double, 5));
-     _beliefBuffer.concat(',');
-  }
+    _beliefBuffer.concat(String(Double, 5));
+    _beliefBuffer.concat(',');
+}
 
 void Communication::sendMessage(){
     Serial.print(_preamble);
@@ -59,3 +61,22 @@ void Communication::sendMessage(){
     _allBeliefs = "";
 }
 
+void Communication::publishROSMessage(ros::Publisher chatter, std_msgs::String str_msg){
+
+    int lenBeliefs = _allBeliefs.length()+2;
+    char lenBeliefsStr[5];
+
+    sprintf(lenBeliefsStr, "%d", lenBeliefs);
+
+    String msg = _preamble + lenBeliefsStr + _start_message + '{' + _allBeliefs + '}' + _end_message;
+
+    _allBeliefs = "";
+
+    int n = msg.length();
+    char char_array[n + 1];
+    strcpy(char_array, msg.c_str());
+
+    str_msg.data = char_array;
+    chatter.publish( &str_msg );
+
+}
--- src/arduino/Embedded_Protocol_2/Embedded_Protocol_2.h
+++ src/arduino/Embedded_Protocol_2/Embedded_Protocol_2.h
@@ -1,34 +1,38 @@
 /*
  *  Embedded_Protocol_2.h
- *  
+ *
  *  T: biblioteca usada pelo atuador (arduino) para mandar os dados "para cima"
  */
 
-#ifndef Embedded_Protocol_2_h    
-#define Embedded_Protocol_2_h 
+#ifndef Embedded_Protocol_2_h
+#define Embedded_Protocol_2_h
 
+#include <ros.h>
+#include <std_msgs/String.h>
 #include "Arduino.h"
 
 class Communication {
 
+
         public:
-        
+
           void startBelief(String beliefName);
           void beliefAdd(String msg);
           void beliefAdd(int Int);
           void beliefAdd(float Float);
-	  void beliefAdd(double Double);
+          void beliefAdd(double Double);
           void endBelief();
           void sendMessage();
-      
+          void publishROSMessage(ros::Publisher chatter, std_msgs::String str_msg);
+
         private:
-	  String _beliefBuffer = "";
-	  String _allBeliefs = "";
-          
+          String _beliefBuffer = "";
+          String _allBeliefs = "";
+
           String _preamble = "==";
           String _start_message = "::";
           String _end_message = "--";
-          
+
 };
 
 #endif
