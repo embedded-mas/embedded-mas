20a170e0cdef449ec88c29a4cf4a0b1ad39c47d8
VitorIntegre <vitor.furio@integrejr.com.br>
2022-07-08 09:18:06 +0000

    teste de push

--- examples/CarExample/src/main/java/DemoEmbeddedAgent.java
+++ examples/CarExample/src/main/java/DemoEmbeddedAgent.java
@@ -20,7 +20,7 @@ public class DemoEmbeddedAgent extends EmbeddedAgent {
 		Arduino4EmbeddedMas arduino = new Arduino4EmbeddedMas("/dev/ttyUSB0",9600);
 		arduino.openConnection();
 		
-		MyDemoDevice device = new MyDemoDevice(new Atom("arduino1"), arduino);
+		CarDevice device = new CarDevice(new Atom("arduino1"), arduino);
 		this.addSensor(device);	
 	}
 	
@@ -29,7 +29,7 @@ public class DemoEmbeddedAgent extends EmbeddedAgent {
 
 	public boolean frente() {
 		//print de monitoramento da ação
-		System.out.println("Fazendo o carro andar para frente");
+		System.out.println("Fazendo o carro andar para frente...");
 		//seleciona os devices e suas atuacoes que que serão utilizados na acao
 		MyDemoDevice device1 =(MyDemoDevice)this.getDevices().get(0);
 		device1.doMotor1Horario();
@@ -37,7 +37,7 @@ public class DemoEmbeddedAgent extends EmbeddedAgent {
 		return true;
 	}
 	public boolean tras() {
-		System.out.println("Fazendo o carro andar para trás");
+		System.out.println("Fazendo o carro andar para trás...");
 		MyDemoDevice device1 =(MyDemoDevice)this.getDevices().get(0);
 		device1.doMotor1Antihorario();
 		device1.doMotor2Antihorario();
@@ -46,29 +46,28 @@ public class DemoEmbeddedAgent extends EmbeddedAgent {
 
 	//Exemplo de como fazer uma mesma acao com atuacoes diferentes:
 	public boolean esquerda() {
-		System.out.println("Fazendo o carro andar para esquerda");
+		System.out.println("Fazendo o carro andar para esquerda...");
 		MyDemoDevice device1 =(MyDemoDevice)this.getDevices().get(0);
 		device1.doMotor1Horario();
 		device1.doMotor2Antihorario();
 		return true;
 	}
 	public boolean esquerda(int a) {
-		System.out.println("Fazendo o carro andar para esquerda");
+		System.out.println("Fazendo o carro andar para esquerda...");
 		MyDemoDevice device1 =(MyDemoDevice)this.getDevices().get(0);
 		device1.doMotor1Horario();
 		device1.doMotor2Para();
 		return true;
 	}
-
 	public boolean direita() {
-		System.out.println("Fazendo o carro andar para direita");
+		System.out.println("Fazendo o carro andar para direita...");
 		MyDemoDevice device1 =(MyDemoDevice)this.getDevices().get(0);
 		device1.doMotor1Antihorario();
 		device1.doMotor2Horario();
 		return true;
 	}
 	public boolean para() {
-		System.out.println("Fazendo o carro andar para para");
+		System.out.println("Fazendo o carro parar...");
 		MyDemoDevice device1 =(MyDemoDevice)this.getDevices().get(0);
 		device1.doMotor1Para();
 		device1.doMotor2Para();
