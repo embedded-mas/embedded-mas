0618a400f5fbe1447f7498e6d4e9e09b83d80d7d
maiquelb <maiquelb@gmail.com>
2023-03-13 13:51:33 -0300

    send on/off values as 1/0 to ROS - issue #15

--- src/main/java/embedded/mas/bridges/ros/ServiceParam.java
+++ src/main/java/embedded/mas/bridges/ros/ServiceParam.java
@@ -13,6 +13,11 @@ public class ServiceParam {
 	public ServiceParam(String paramName, Object paramValue){
 		super();
 		this.paramName = paramName;
+		if(paramValue=="on") 
+			this.paramValue = 0;
+		else
+			if(paramValue=="off") 
+				this.paramValue = 0;
 		this.paramValue = paramValue;				
 	}
 
@@ -45,8 +50,15 @@ public class ServiceParam {
 				pValue = ((ServiceParameters)paramValue).toJson().toString();
 			}
 			else
-				if(paramValue instanceof String) 
-					pValue = pValue + "\"" + paramValue.toString() + "\"" ;
+				if(paramValue instanceof String) {					
+					if(paramValue.equals("on"))  //on/off values must be translated to 1/0
+						pValue = "1";
+					else
+						if(paramValue.equals("off"))  
+							pValue = "0";
+						else 								
+							pValue = pValue + "\"" + paramValue.toString() + "\"" ;
+				}
 				else
 					pValue = paramValue.toString();
 		return "\""+ paramName + "\":" + pValue ; 
