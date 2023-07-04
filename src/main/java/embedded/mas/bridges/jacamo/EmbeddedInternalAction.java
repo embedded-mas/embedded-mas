ef402c3b6c43cf21cd21367c631f9ada73881bb9
maiquelb <maiquelb@gmail.com>
2023-07-03 23:25:00 -0300

    adapt concurrent embedded internal actions to EmbeddedActions (from yaml config files)

--- src/main/java/embedded/mas/bridges/jacamo/EmbeddedInternalAction.java
+++ src/main/java/embedded/mas/bridges/jacamo/EmbeddedInternalAction.java
@@ -1,7 +1,16 @@
 package embedded.mas.bridges.jacamo;
 
 import jason.asSemantics.DefaultInternalAction;
+import jason.asSyntax.StringTermImpl;
+import jason.asSyntax.Term;
 
 public abstract class EmbeddedInternalAction extends DefaultInternalAction{
+	
+	protected Term adaptTerm(Term t) {
+		if(t.toString().matches("\"(.+)\"")) 
+			return new StringTermImpl(t.toString().replaceAll("\"(.+)\"", "$1")); 
+		return t;
+		
+	}
 
 }
