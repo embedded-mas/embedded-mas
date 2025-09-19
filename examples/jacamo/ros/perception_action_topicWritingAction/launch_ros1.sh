63adbc191175c89f7a8c0ff6a195d12a6c21b359
maiquelb <maiquelb@gmail.com>
2025-09-19 15:23:35 -0300

    fix ros setup of topic writing example

--- examples/jacamo/ros/perception_action_topicWritingAction/launch_ros1.sh
+++ examples/jacamo/ros/perception_action_topicWritingAction/launch_ros1.sh
@@ -8,9 +8,10 @@ set -e
 ( docker network inspect ros >/dev/null 2>&1 ||  docker network create ros)
 ( docker volume inspect x11socket >/dev/null 2>&1 ||  docker volume create x11socket)
 
- docker run -d --rm --net=ros     --env="DISPLAY_WIDTH=3000"     --env="DISPLAY_HEIGHT=1800"     --env="RUN_XTERM=no"     --name=novnc -p=8080:8080 theasp/novnc:latest
 
- docker run --rm -d --net=ros     --env="DISPLAY=novnc:0.0"     --env="ROS_MASTER_URI=http://embedded-mas-example:11311"     --name embedded-mas-example     -p 9090:9090     maiquelb/embedded-mas-ros:latest 
+ docker run --rm -d --net=ros      --env="ROS_MASTER_URI=http://embedded-mas-example:11311"     --name embedded-mas-example     -p 9090:9090     maiquelb/embedded-mas-ros:latest 
+
+echo -e "\e[1;33m**** Launching ROS container. Wait 20 seconds ****\e[0m"	
 
  sleep 20
 
@@ -20,4 +21,4 @@ docker exec -d embedded-mas-example /bin/bash -c " source /opt/ros/noetic/setup.
 docker exec -d embedded-mas-example /bin/bash -c " source /opt/ros/noetic/setup.bash &&  rostopic pub  --latch  /current_time std_msgs/String 'unknown' "
 
 
-echo -e "\e[1;33m**** Docker container is ready. Start the JaCaMo application ****\e[0m"	
+echo -e "\e[1;33m**** ROS container is ready. Start the JaCaMo application ****\e[0m"	
