b8e60e1dd77cf286eee69108ed0b76574e317496
maiquelb <maiquelb@gmail.com>
2025-09-19 13:33:18 -0300

    fix ros setup of topic writing example

--- examples/jacamo/ros/perception_action_topicWritingAction/launch_ros1.sh
+++ examples/jacamo/ros/perception_action_topicWritingAction/launch_ros1.sh
@@ -1,18 +1,23 @@
-# Encerrar e limpar containers se estiverem rodando
-for cname in noetic; do
-    if docker ps -q --filter "name=${cname}" | grep -q .; then
-        echo "Stopping existing container: ${cname}"
-        docker stop "${cname}"
-    fi
-done
+#!/bin/bash
+set -e
 
-docker run -it -p9090:9090 --rm --net=ros --name noetic maiquelb/embedded-mas-ros:latest \
-/bin/bash -c " ((source /opt/ros/noetic/setup.bash &&roslaunch rosbridge_server rosbridge_websocket.launch) & \
-             (echo -e '\e[1;33m**** Launching the Docker container. Wait a few seconds...****\e[0m]'  && \
-              sleep 5 && \
-              source /opt/ros/noetic/setup.bash && \
-              (rostopic pub /value1 std_msgs/Int32 0  > /dev/null 2>&1 & \
-               rostopic pub /current_time std_msgs/String 'unknown'  > /dev/null 2>&1 &)&&\
-              echo -e '\e[1;33m**** Docker container is ready. Start the JaCaMo application****\e[0m]' && \
-              tail -f /dev/null  ))"
-           
+# stop running containers
+(docker ps -q --filter "name=embedded-mas-example" | grep -q . && docker stop embedded-mas-example || true)
+
+
+( docker network inspect ros >/dev/null 2>&1 ||  docker network create ros)
+( docker volume inspect x11socket >/dev/null 2>&1 ||  docker volume create x11socket)
+
+ docker run -d --rm --net=ros     --env="DISPLAY_WIDTH=3000"     --env="DISPLAY_HEIGHT=1800"     --env="RUN_XTERM=no"     --name=novnc -p=8080:8080 theasp/novnc:latest
+
+ docker run --rm -d --net=ros     --env="DISPLAY=novnc:0.0"     --env="ROS_MASTER_URI=http://embedded-mas-example:11311"     --name embedded-mas-example     -p 9090:9090     maiquelb/embedded-mas-ros:latest 
+
+ sleep 20
+
+
+docker exec -d embedded-mas-example /bin/bash -c " source /opt/ros/noetic/setup.bash &&  rostopic pub  --latch /value1 std_msgs/Int32 0"
+
+docker exec -d embedded-mas-example /bin/bash -c " source /opt/ros/noetic/setup.bash &&  rostopic pub  --latch  /current_time std_msgs/String 'unknown' "
+
+
+echo -e "\e[1;33m**** Docker container is ready. Start the JaCaMo application ****\e[0m"	
